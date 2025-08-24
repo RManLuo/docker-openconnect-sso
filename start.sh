d096417d79e4e3657dcc65bc5b69fb1c142b1973
Linhao Luo <34737002+RManLuo@users.noreply.github.com>
2025-08-24 21:18:34 +1000

    Add conditional authgroup handling in start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,9 @@ iptables -I INPUT -p tcp --dport 1080 -j ACCEPT && iptables-save
 
 /usr/bin/supervisord
 
-sudo QT_QUICK_BACKEND=software OPENSSL_CONF=/openssl.cnf QTWEBENGINE_DISABLE_SANDBOX=1 openconnect-sso --server ${SERVER_NAME} --user ${USER_NAME} --authgroup ${AUTHGROUP}
+# Add authgroup only if set
+if [ -n "${AUTHGROUP}" ]; then
+  sudo QT_QUICK_BACKEND=software OPENSSL_CONF=/openssl.cnf QTWEBENGINE_DISABLE_SANDBOX=1 openconnect-sso --server ${SERVER_NAME} --user ${USER_NAME} --authgroup ${AUTHGROUP}
+else
+  sudo QT_QUICK_BACKEND=software OPENSSL_CONF=/openssl.cnf QTWEBENGINE_DISABLE_SANDBOX=1 openconnect-sso --server ${SERVER_NAME} --user ${USER_NAME}
+fi
